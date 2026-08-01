548180cb7dee586bad490aeceb91c1e6ba4da453
SRINATH-sketch <gsrinath222@gmail.com>
2026-08-01 22:40:03 +0530

    Time: 3 ms (16.15%), Space: 19.1 MB (96.79%) - LeetHub

--- 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.py
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.py
@@ -1,4 +1,5 @@
 class Solution:
     def theMaximumAchievableX(self, num: int, t: int) -> int:
-        r=num+2*t
-        return r
+        for i in range(t):
+            num=num+1
+        return num+t
