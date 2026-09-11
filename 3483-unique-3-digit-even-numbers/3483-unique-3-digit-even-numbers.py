0060156eb4ceadb7b415c5e8e3bd3ca1ace823e0
SRINATH-sketch <gsrinath222@gmail.com>
2026-09-11 10:17:04 +0530

    Time: 16 ms (61.32%), Space: 19 MB (99.56%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.py
@@ -1,8 +1,10 @@
-from itertools import permutations
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        count=set()
-        for i in permutations(digits,3):
-            if(i[0]!=0 and i[2]%2==0):
-                count.add(i)
-        return len(count)
+        c1=set()
+        for i in range(len(digits)):
+            for j in range(len(digits)):
+                for k in range(len(digits)):
+                    if(i!=j and j!=k and k!=i):
+                        if(digits[i]!=0 and digits[k]%2==0):
+                            c1.add((digits[i],digits[j],digits[k]))
+        return len(c1)
